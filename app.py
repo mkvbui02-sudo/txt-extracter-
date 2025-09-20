2b75feb8f1e32a10d98d20500a6e201b3c8072d0
mkvbui02-sudo <mkvbui02@gmail.com>
2025-09-20 19:17:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
-PORT = int(os.environ.get("PORT", 8080))
+PORT = int(os.environ.get("PORT", 8000))
 
 @app.route("/")
 def home():
     return "Hello, Render!"
 
 if __name__ == "__main__":
-    app.run(host="8.0.8.0", port=PORT)
+    app.run(host="8.0.0.0", port=PORT)
